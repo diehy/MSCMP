ee0d8f5cdbd962b87749db7983ea38d95da15a37
diehy <gutmanis@protonmail.com>
2025-12-29 12:26:22 +0000

    Nuked original client code as it was a prototype

---
+++ src/MSCMP/MSCMP.cs
@@ -0,0 +1,19 @@
+﻿using MSCLoader;
+
+namespace MSCMP
+{
+    /// <summary> Main mod class. </summary>
+    public class MSCMP : Mod
+    {
+        /// <summary> Unique mod ID for this. </summary>
+        public override string ID => "MSCMP";
+        /// <summary> Full mod name. </summary>
+        public override string Name => "My Summer Car - Multiplayer";
+        /// <summary> Local version. </summary>
+        public override string Version => "1.0";
+        /// <summary> Main author/contributors. </summary>
+        public override string Author => "diehy";
+        /// <summary> Short description of the mod. </summary>
+        public override string Description => "Cooperative multiplayer mod for My Summer Car.";
+    }
+}
